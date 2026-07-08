51450c06bf431217e076d21472a317e4027d777c
hama210 <65832111+hama210@users.noreply.github.com>
2026-07-08 16:24:55 +0300

    Add language config

---
+++ src/config/languages.js
@@ -0,0 +1,7 @@
+export const languages = [
+  { code: 'KU', label: 'کوردی', flag: '☀️', direction: 'rtl' },
+  { code: 'EN', label: 'English', flag: '🇬🇧', direction: 'ltr' },
+  { code: 'AR', label: 'العربية', flag: '🇸🇦', direction: 'rtl' }
+];
+
+export const defaultLanguage = 'KU';
